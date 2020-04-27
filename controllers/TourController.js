ac67424eaf20f1195e24e76166fe58c69743f8bd
Muhamad Hafiz <muhamadhfz@gmail.com>
2020-04-27 16:22:20 +0700

    established connection to mongoDB with mongoose. Cleaning the TourController to not depend on the previous resource anymore

--- controllers/TourController.js
+++ controllers/TourController.js
@@ -1,20 +1,4 @@
-const fs = require('fs')
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-)
-
-exports.checkId = (req, res, next, val) => {
-  console.log(`Tour ID is: ${val}`)
-
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID / No tour found for update',
-    })
-  }
-  next()
-}
+const Tour = require('../models/TourModel')
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -31,10 +15,10 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   })
 }
 
@@ -42,45 +26,31 @@ exports.getTour = (req, res) => {
   console.log(req.params)
   const paramID = req.params.id * 1
 
-  if (paramID > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID / No tour found',
-    })
-  }
+  // if (paramID > tours.length) {
+  //   return res.status(404).json({
+  //     status: 'fail',
+  //     message: 'Invalid ID / No tour found',
+  //   })
+  // }
 
-  const tour = tours.find((el) => el.id === paramID)
+  // const tour = tours.find((el) => el.id === paramID)
 
-  res.status(200).json({ status: 'success', data: { tour } })
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: { tour },
+  // })
 }
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1
-  const newTour = Object.assign(req.body, { id: newId })
-
-  tours.push(newTour)
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      })
-    }
-  )
+  res.status(201).json({
+    status: 'success',
+    // data: {
+    //   tour: newTour,
+    // },
+  })
 }
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID / No tour found for update',
-    })
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
