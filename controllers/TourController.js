32e2c2bfb7c86c0c5c1e2b49f0c9d8718b63dde9
Muhamad Hafiz <muhamadhfz@gmail.com>
2020-04-28 17:26:35 +0700

    add projection and simple paginate functionality in GET all tours request

--- controllers/TourController.js
+++ controllers/TourController.js
@@ -2,14 +2,14 @@ const Tour = require('../models/TourModel')
 
 exports.getAllTours = async (req, res) => {
   try {
-    //*  #1a Basic Filtering
+    //*  (#1) ##### Filtering
 
+    //  #1a Basic Filtering
     const queryObj = { ...req.query } //from url
     const excludedFields = ['page', 'sort', 'limit', 'fields'] //query string we want to remove from queryObj
     excludedFields.forEach((el) => delete queryObj[el]) //delete the excluded keyword from the queryObj
 
-    //*  #1b Advance ##### Filtering
-
+    //  #1b Advance Filtering
     let queryStr = JSON.stringify(queryObj) //ex: { "duration": {"gte": "10"} }
 
     queryStr = queryStr.replace(
@@ -29,6 +29,30 @@ exports.getAllTours = async (req, res) => {
       query = query.sort(sortBy)
     } else {
       query = query.sort('-createdAt')
+    }
+
+    //*   (#3) Limiting output fields
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ')
+      query = query.select(fields)
+    } else {
+      query.select('-__v') //excluding the mongoose property
+    }
+
+    //*   (#4) Pagination
+
+    const page = req.query.page * 1 || 1
+    const limit = req.query.limit * 1 || 100
+    const skippedDocs = (page - 1) * limit
+
+    query = query.skip(skippedDocs).limit(limit)
+
+    if (req.query.page) {
+      const tourTotal = await Tour.countDocuments()
+      if (skippedDocs >= tourTotal) {
+        throw new Error('Page you requested didnt exist!')
+      }
     }
 
     //*   EXECUTE the final form of query
