ab2a61ebd3bbb67d620a2ce1444711030627d9f8
Muhamad Hafiz <muhamadhfz@gmail.com>
2020-05-19 17:31:32 +0700

    fixing error on user model file

--- server.js
+++ server.js
@@ -9,23 +9,8 @@ process.on('uncaughtException', (err) => {
   process.exit(1)
 })
 
-const app = require('./app')
-
-/*
-MAILTRAP_USERNAME=c311cff290b08c
-MAILTRAP_PASSWORD=c41cda4dd59f9c
-MAILTRAP_HOST=smtp.mailtrap.io
-MAILTRAP_PORT=25
-EMAIL_FROM=hafizmhmd9@gmail.com
-SENDGRID_USERNAME=apikey
-SENDGRID_PASSWORD=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
-SENDGRID_HOST=smtp.sendgrid.net
-SENDGRID_PORT=25
-STRIPE_SECRET_KEY=sk_test_4MP3bBTU497ZnOEpT8XMbwnw00D3iBHwwH
-*/
-
 dotenv.config({ path: './config.env' })
-
+const app = require('./app')
 
 const DB = process.env.DATABASE.replace(
   '<password>',
