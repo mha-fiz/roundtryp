93f6d7b6aa432729953d2a0eeeaf9195bf0229d7
Muhamad Hafiz <muhamadhfz@gmail.com>
2020-06-04 20:34:09 +0700

    view route bug fix

--- routes/ViewRoutes.js
+++ routes/ViewRoutes.js
@@ -1,25 +1,14 @@
 const express = require('express')
 const viewController = require('../controllers/ViewController')
 const authController = require('../controllers/AuthController')
-const bookingController = require('../controllers/BookingController')
 
 const router = express.Router()
 
-router.get(
-  '/',
-  bookingController.createBookingCheckout,
-  authController.isLoggedIn,
-  viewController.getOverview
-)
+router.get('/', authController.isLoggedIn, viewController.getOverview)
 router.get('/signup', viewController.signup)
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour)
 router.get('/login', authController.isLoggedIn, viewController.login)
 router.get('/me', authController.protect, viewController.getAccount)
-router.get(
-  '/my-tours',
-  // bookingController.createBookingCheckout,
-  authController.protect,
-  viewController.getMyTours
-)
+router.get('/my-tours', authController.protect, viewController.getMyTours)
 
 module.exports = router
